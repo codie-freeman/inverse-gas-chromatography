309965fb227b9261715bcd05d33228765378eac6
Codie <codie.freeman02@gmail.com>
2026-01-31 22:23:31 +0000

    Add plotting and utility module placeholders
    - Create structure for future plotting utilities
    - Add empty utils module for shared functions

---
+++ src/igcsea/plotting/__init__.py
@@ -0,0 +1,4 @@
+"""Plotting utilities for IGC-SEA data visualization."""
+
+# Will be populated as plotting modules are created
+__all__ = []
---
+++ src/igcsea/utils/__init__.py
@@ -0,0 +1,3 @@
+"""Utility functions for IGC-SEA package."""
+
+__all__ = []
